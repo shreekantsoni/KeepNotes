7b8f391acd11d87dbe7919c4f3573426b6672bbe
Shree <srikantsoni11@gmail.com>
2020-09-04 21:18:17 +0530

    Updates

---
+++ precache-manifest.1a4e7ff0d0715f5a7cc9cb94e52f3cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f510f7d3ba01abb85395786e5d006c",
+    "url": "/KeepNotes/index.html"
+  },
+  {
+    "revision": "cecaf19e09439b050484",
+    "url": "/KeepNotes/static/css/main.bd2bcfb7.chunk.css"
+  },
+  {
+    "revision": "9e322ee6138073aac619",
+    "url": "/KeepNotes/static/js/2.27944b84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/KeepNotes/static/js/2.27944b84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cecaf19e09439b050484",
+    "url": "/KeepNotes/static/js/main.49914561.chunk.js"
+  },
+  {
+    "revision": "06fe781579c45478e6ae",
+    "url": "/KeepNotes/static/js/runtime-main.95f8598e.js"
+  }
+]);
